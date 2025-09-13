e746c9e1ac9dc213512469c6271daf3407f0c795
Jay <rright815@gmail.com>
2025-09-13 12:29:28 +0800

    168

--- src/app/temperature.pipe.ts
+++ src/app/temperature.pipe.ts
@@ -6,10 +6,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TemperaturePipe implements PipeTransform {
   transform(
-    value: string | number,
+    value: string | number | null,
     inputType: 'cel' | 'fah',
     outputType?: 'cel' | 'fah'
   ) {
+    if(!value){
+        return value;
+    }
     let val: number;
 
     if (typeof value === 'string') {
@@ -35,6 +38,6 @@ export class TemperaturePipe implements PipeTransform {
       symbol = outputType === 'cel' ? '°C' : '°F';
     }
 
-    return `${outputTemp} ${symbol}`;
+    return `${outputTemp.toFixed(2)} ${symbol}`;
   }
 }
